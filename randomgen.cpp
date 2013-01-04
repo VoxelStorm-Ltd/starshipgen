719694fb4ed7f4b4b8cb617943ec3578079b6993
SlowRiot <rain.backnet@gmail.com>
2013-01-04 11:37:31 +0000

    additional random functions - biased normal distributions and multi-type pickers

--- randomgen.cpp
+++ randomgen.cpp
@@ -26,7 +26,23 @@ char randomgenerator::random_char() {
 }
 char randomgenerator::random_char_normal() {
   boost::random::normal_distribution<> randomdist(0.0, 64.0);
-  return randomdist(gen);
+  double thisresult = randomdist(gen);
+  if(thisresult > 127) {
+    thisresult = 127;
+  } else if(thisresult < -127) {
+    thisresult = -127;
+  }
+  return thisresult;
+}
+char randomgenerator::random_char_normal_biased(char bias) {
+  boost::random::normal_distribution<> randomdist(bias, 64.0);
+  double thisresult = randomdist(gen);
+  if(thisresult > 127) {
+    thisresult = 127;
+  } else if(thisresult < -127) {
+    thisresult = -127;
+  }
+  return thisresult;
 }
 
 unsigned char randomgenerator::random_uchar() {
@@ -35,5 +51,836 @@ unsigned char randomgenerator::random_uchar() {
 }
 unsigned char randomgenerator::random_uchar_normal() {
   boost::random::normal_distribution<> randomdist(127.0, 64.0);
-  return randomdist(gen);
+  double thisresult = randomdist(gen);
+  if(thisresult > 255) {
+    thisresult = 255;
+  } else if(thisresult < 0) {
+    thisresult = 0;
+  }
+  return thisresult;
+}
+unsigned char randomgenerator::random_uchar_normal_biased(unsigned char bias) {
+  boost::random::normal_distribution<> randomdist(bias, 64.0);
+  double thisresult = randomdist(gen);
+  if(thisresult > 255) {
+    thisresult = 255;
+  } else if(thisresult < 0) {
+    thisresult = 0;
+  }
+  return thisresult;
+}
+
+char randomgenerator::pick_one(char c1, char c2) {
+  boost::random::uniform_smallint<> randomdist(1, 2);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3) {
+  boost::random::uniform_smallint<> randomdist(1, 3);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4) {
+  boost::random::uniform_smallint<> randomdist(1, 4);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5) {
+  boost::random::uniform_smallint<> randomdist(1, 5);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5, char c6) {
+  boost::random::uniform_smallint<> randomdist(1, 6);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7) {
+  boost::random::uniform_smallint<> randomdist(1, 7);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8) {
+  boost::random::uniform_smallint<> randomdist(1, 8);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8, char c9) {
+  boost::random::uniform_smallint<> randomdist(1, 9);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  }
+  return c1;
+}
+char randomgenerator::pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8, char c9, char c10) {
+  boost::random::uniform_smallint<> randomdist(1, 10);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  case 10:
+    return c10;
+  }
+  return c1;
+}
+
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2) {
+  boost::random::uniform_smallint<> randomdist(1, 2);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3) {
+  boost::random::uniform_smallint<> randomdist(1, 3);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4) {
+  boost::random::uniform_smallint<> randomdist(1, 4);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5) {
+  boost::random::uniform_smallint<> randomdist(1, 5);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6) {
+  boost::random::uniform_smallint<> randomdist(1, 6);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7) {
+  boost::random::uniform_smallint<> randomdist(1, 7);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8) {
+  boost::random::uniform_smallint<> randomdist(1, 8);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8, unsigned char c9) {
+  boost::random::uniform_smallint<> randomdist(1, 9);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  }
+  return c1;
+}
+unsigned char randomgenerator::pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8, unsigned char c9, unsigned char c10) {
+  boost::random::uniform_smallint<> randomdist(1, 10);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  case 10:
+    return c10;
+  }
+  return c1;
+}
+
+int randomgenerator::pick_one(int c1, int c2) {
+  boost::random::uniform_smallint<> randomdist(1, 2);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3) {
+  boost::random::uniform_smallint<> randomdist(1, 3);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4) {
+  boost::random::uniform_smallint<> randomdist(1, 4);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5) {
+  boost::random::uniform_smallint<> randomdist(1, 5);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5, int c6) {
+  boost::random::uniform_smallint<> randomdist(1, 6);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7) {
+  boost::random::uniform_smallint<> randomdist(1, 7);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8) {
+  boost::random::uniform_smallint<> randomdist(1, 8);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8, int c9) {
+  boost::random::uniform_smallint<> randomdist(1, 9);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  }
+  return c1;
+}
+int randomgenerator::pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8, int c9, int c10) {
+  boost::random::uniform_smallint<> randomdist(1, 10);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  case 10:
+    return c10;
+  }
+  return c1;
+}
+
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2) {
+  boost::random::uniform_smallint<> randomdist(1, 2);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3) {
+  boost::random::uniform_smallint<> randomdist(1, 3);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4) {
+  boost::random::uniform_smallint<> randomdist(1, 4);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5) {
+  boost::random::uniform_smallint<> randomdist(1, 5);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6) {
+  boost::random::uniform_smallint<> randomdist(1, 6);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7) {
+  boost::random::uniform_smallint<> randomdist(1, 7);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8) {
+  boost::random::uniform_smallint<> randomdist(1, 8);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8, unsigned int c9) {
+  boost::random::uniform_smallint<> randomdist(1, 9);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  }
+  return c1;
+}
+unsigned int randomgenerator::pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8, unsigned int c9, unsigned int c10) {
+  boost::random::uniform_smallint<> randomdist(1, 10);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  case 10:
+    return c10;
+  }
+  return c1;
+}
+
+std::string randomgenerator::pick_one(std::string c1, std::string c2) {
+  boost::random::uniform_smallint<> randomdist(1, 2);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3) {
+  boost::random::uniform_smallint<> randomdist(1, 3);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4) {
+  boost::random::uniform_smallint<> randomdist(1, 4);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5) {
+  boost::random::uniform_smallint<> randomdist(1, 5);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6) {
+  boost::random::uniform_smallint<> randomdist(1, 6);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7) {
+  boost::random::uniform_smallint<> randomdist(1, 7);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8) {
+  boost::random::uniform_smallint<> randomdist(1, 8);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8, std::string c9) {
+  boost::random::uniform_smallint<> randomdist(1, 9);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  }
+  return c1;
+}
+std::string randomgenerator::pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8, std::string c9, std::string c10) {
+  boost::random::uniform_smallint<> randomdist(1, 10);
+  switch(randomdist(gen)) {
+  case 1:
+    return c1;
+  case 2:
+    return c2;
+  case 3:
+    return c3;
+  case 4:
+    return c4;
+  case 5:
+    return c5;
+  case 6:
+    return c6;
+  case 7:
+    return c7;
+  case 8:
+    return c8;
+  case 9:
+    return c9;
+  case 10:
+    return c10;
+  }
+  return c1;
 }
--- randomgen.h
+++ randomgen.h
@@ -1,6 +1,7 @@
 #ifndef RANDOMGEN_H_INCLUDED
 #define RANDOMGEN_H_INCLUDED
 
+#include <string>
 #include <boost/random/mersenne_twister.hpp>
 
 class randomgenerator {
@@ -14,8 +15,56 @@ public:
   bool random_bool_weighted(double weight_true);
   char random_char();
   char random_char_normal();
+  char random_char_normal_biased(char bias);
   unsigned char random_uchar();
   unsigned char random_uchar_normal();
+  unsigned char random_uchar_normal_biased(unsigned char bias);
+
+  char pick_one(char c1, char c2);
+  char pick_one(char c1, char c2, char c3);
+  char pick_one(char c1, char c2, char c3, char c4);
+  char pick_one(char c1, char c2, char c3, char c4, char c5);
+  char pick_one(char c1, char c2, char c3, char c4, char c5, char c6);
+  char pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7);
+  char pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8);
+  char pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8, char c9);
+  char pick_one(char c1, char c2, char c3, char c4, char c5, char c6, char c7, char c8, char c9, char c10);
+  unsigned char pick_one(unsigned char c1, unsigned char c2);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8, unsigned char c9);
+  unsigned char pick_one(unsigned char c1, unsigned char c2, unsigned char c3, unsigned char c4, unsigned char c5, unsigned char c6, unsigned char c7, unsigned char c8, unsigned char c9, unsigned char c10);
+  int pick_one(int c1, int c2);
+  int pick_one(int c1, int c2, int c3);
+  int pick_one(int c1, int c2, int c3, int c4);
+  int pick_one(int c1, int c2, int c3, int c4, int c5);
+  int pick_one(int c1, int c2, int c3, int c4, int c5, int c6);
+  int pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7);
+  int pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8);
+  int pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8, int c9);
+  int pick_one(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8, int c9, int c10);
+  unsigned int pick_one(unsigned int c1, unsigned int c2);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8, unsigned int c9);
+  unsigned int pick_one(unsigned int c1, unsigned int c2, unsigned int c3, unsigned int c4, unsigned int c5, unsigned int c6, unsigned int c7, unsigned int c8, unsigned int c9, unsigned int c10);
+  std::string pick_one(std::string c1, std::string c2);
+  std::string pick_one(std::string c1, std::string c2, std::string c3);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8, std::string c9);
+  std::string pick_one(std::string c1, std::string c2, std::string c3, std::string c4, std::string c5, std::string c6, std::string c7, std::string c8, std::string c9, std::string c10);
 };
 
 #endif // RANDOMGEN_H_INCLUDED

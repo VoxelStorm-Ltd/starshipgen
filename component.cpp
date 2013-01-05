a08ca2222b0d06c8ba55b142408aedba964de6da
SlowRiot <rain.backnet@gmail.com>
2013-01-05 11:52:17 +0000

    power cycling and install time tracking, adjectives for components

--- component.cpp
+++ component.cpp
@@ -1,11 +1,17 @@
 #include "component.h"
+#include "componenttype.h"
+#include "starship.h"
+#include "randomgen.h"
 
-component::component(componenttype *thiscomponenttype) {
+component::component(componenttype *thiscomponenttype, starship *thisship) {
   thistype = thiscomponenttype;
 
-  timeoffailure = thistype->mtbf + rand() % thistype->mtbfvariation;
-  failurestate  = componenttype::STATE_OK;
-  failuremode   = componenttype::FAILURE_NONE;
+  timeinstalled    = thisship->shipclock;
+  timelastswitched = 0;   // never activated
+  powercycles      = 0;
+  timeoffailure    = timeinstalled + (thistype->mtbf + rand() % thistype->mtbfvariation);
+  failurestate     = componenttype::STATE_OK;
+  failuremode      = componenttype::FAILURE_NONE;
 
   temperature = 294.15;     // 21 deg C in K
 }
@@ -13,3 +19,24 @@ component::component(componenttype *thiscomponenttype) {
 component::~component() {
 }
 
+std::string component::get_adjective() {
+  switch(failurestate) {
+  case componenttype::STATE_OK:
+    return "";
+  case componenttype::STATE_WORN:
+    return "worn";
+  case componenttype::STATE_REDUCEDFUNCTION:
+    return "glitchy";
+  case componenttype::STATE_NONFUNCTIONAL:
+    return "broken";
+  case componenttype::STATE_REPAIRING:
+    return "disassembled";
+  case componenttype::STATE_DESTROYED:
+    return "destroyed";
+  case componenttype::STATE_ABSENT:
+    return "missing";
+  default:
+    std::cout << "ERROR: get_adjective: unknown failure state enum " << failurestate << std::endl;
+  }
+  return "";
+}
